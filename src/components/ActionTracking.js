cdd9257e246fe05aac7a4ed601915cb3e0a12e6f
Jared <126293208+indohippi@users.noreply.github.com>
2023-11-01 18:35:47 -0500

    managemnet updates

--- src/components/ActionTracking.js
+++ src/components/ActionTracking.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ActionTracking = ({ currentGame, actions, setActions }) => {
+const ActionTracking = ({ players = [] }) => { // Default value for players prop
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [actionType, setActionType] = useState('');
+  const [location, setLocation] = useState('');
+
+  const handleActionSubmit = (e) => {
+    e.preventDefault();
+    setActionType('');
+    setLocation('');
+  };
+
   return (
     <div>
       <h2>Action Tracking</h2>
-      {/* Add functionality to track actions like shots taken, missed, made, and their location */}
+      <form onSubmit={handleActionSubmit}>
+        <div>
+          <label>Select Player: </label>
+          <select value={selectedPlayer} onChange={(e) => setSelectedPlayer(e.target.value)}>
+            <option value="">--Select--</option>
+            {Array.isArray(players) && players.map(player => ( // Check if players is an array
+              <option key={player.id} value={player.id}>
+                {player.name} - {player.jerseyNumber}
+              </option>
+            ))}
+          </select>
+        </div>
+        {/* ... rest of the component ... */}
+      </form>
     </div>
   );
 };
--- src/components/GameManagement.js
+++ src/components/GameManagement.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const GameManagement = ({ currentGame, setCurrentGame, players }) => {
+const GameManagement = ({ players }) => {
+  const [activeGame, setActiveGame] = useState(false);
+  const [selectedPlayers, setSelectedPlayers] = useState([]);
+
+  const startNewGame = () => {
+    setActiveGame(true);
+  };
+
+  const endGame = () => {
+    setActiveGame(false);
+    setSelectedPlayers([]);
+  };
+
+  const togglePlayerSelection = (playerId) => {
+    if (selectedPlayers.includes(playerId)) {
+      setSelectedPlayers(prevPlayers => prevPlayers.filter(id => id !== playerId));
+    } else {
+      setSelectedPlayers(prevPlayers => [...prevPlayers, playerId]);
+    }
+  };
+
   return (
     <div>
       <h2>Game Management</h2>
-      {!currentGame ? (
-        <button onClick={() => setCurrentGame({ players: [], status: 'ongoing' })}>Start New Game</button>
+      {!activeGame ? (
+        <button onClick={startNewGame}>Start New Game</button>
       ) : (
         <>
-          <button onClick={() => setCurrentGame(null)}>End Game</button>
-          {/* Add player selection for the game here */}
+          <button onClick={endGame}>End Game</button>
+          <h3>Select Players for the Game</h3>
+          <ul>
+            {players.map(player => (
+              <li key={player.id}>
+                <label>
+                  <input 
+                    type="checkbox" 
+                    checked={selectedPlayers.includes(player.id)} 
+                    onChange={() => togglePlayerSelection(player.id)}
+                  />
+                  {player.name} - {player.jerseyNumber}
+                </label>
+              </li>
+            ))}
+          </ul>
         </>
       )}
     </div>
