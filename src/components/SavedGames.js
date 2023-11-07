6e7293cc1f827d30ca5f8c15ed59bfcc54837ec2
Jared <126293208+indohippi@users.noreply.github.com>
2023-11-07 16:18:24 -0600

    added pdf export

--- src/components/SavedGames.js
+++ src/components/SavedGames.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
+import jsPDF from 'jspdf';
 
 const SavedGames = ({ games }) => {
   const [expandedGameId, setExpandedGameId] = useState(null);
 
   const toggleGameDetails = (gameId) => {
     setExpandedGameId(expandedGameId === gameId ? null : gameId);
+  };
+
+  const exportToPDF = (game) => {
+    const doc = new jsPDF();
+
+    doc.text(`Game ID: ${game.id}`, 10, 10);
+    doc.text('Player Stats:', 10, 20);
+    game.playerStats.forEach((stat, index) => {
+      doc.text(`${stat.name}: Goals: ${stat.stats.goals}, Assists: ${stat.stats.assists}`, 10, 30 + (index * 10));
+    });
+    doc.text(`Team Stats:`, 10, 30 + (game.playerStats.length * 10));
+    doc.text(`Total Goals: ${game.teamStats.goals}`, 10, 40 + (game.playerStats.length * 10));
+    doc.text(`Total Assists: ${game.teamStats.assists}`, 10, 50 + (game.playerStats.length * 10));
+
+    doc.save(`game_${game.id}.pdf`);
   };
 
   // Ensure that games is always an array
@@ -38,6 +54,7 @@ const SavedGames = ({ games }) => {
               </div>
             </div>
           )}
+          <button onClick={() => exportToPDF(game)}>Export to PDF</button>
         </div>
       ))}
     </div>
